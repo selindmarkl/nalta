55b4473abb923b79d5491b76215cd6bb45d65c52
selindmarkl <lars.lindmark@jidoka.se>
2013-04-05 20:20:54 +0300

    Update nalta meta.php

--- plugins/nalta-meta/nalta meta.php
+++ plugins/nalta-meta/nalta meta.php
@@ -5,6 +5,6 @@ Plugin URI: http://yellotab.se
 Description: Här beskriver jag vad mitt plugin skall göra och vad användaren har för nytta av det
 Version: 0.5
 Author: Jido 
-Author URI: http://kiube.se/p007/
+Author URI: https://github.com/selindmarkl
 */
 ?>
