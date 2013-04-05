42425b82e2ccdea82ccf713db806fbaf5fb8636d
selindmarkl <lars.lindmark@jidoka.se>
2013-04-05 20:30:51 +0300

    Update nalta meta.php

--- plugins/nalta-meta/nalta meta.php
+++ plugins/nalta-meta/nalta meta.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Nalta meta till fotografier
-Plugin URI: http://yellotab.se
+Plugin URI: https://github.com/selindmarkl/nalta/tree/master/plugins/nalta-meta
 Description: Här beskriver jag vad mitt plugin skall göra och vad användaren har för nytta av det
 Version: 0.5
 Author: Jido 
